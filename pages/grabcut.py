7c9b3e30e581f384632bdb9047a685c8e6c44a5a
MinTiu03 <your-email@example.com>
2024-12-23 16:17:40 +0700

    Cập nhật các tính năng xử lý ảnh: thêm kỹ thuật flip, xoay, tịnh tiến, cắt và chuyển đổi màu

--- pages/grabcut.py
+++ pages/grabcut.py
@@ -1,5 +1,7 @@
 from PIL import Image
 import streamlit as st
+from streamlit_drawable_canvas import st_canvas, CanvasResult
+
 from components.grabcut import (
     display_form_draw,
     display_guide,
@@ -9,23 +11,62 @@ from components.grabcut import (
 )
 from services.grabcut.ultis import get_object_from_st_canvas
 
-init_session_state()
+def init_session_state():
+    keys = ["final_mask", "result_grabcut"]
+    for key in keys:
+        if key not in st.session_state:
+            st.session_state[key] = None
+def display_st_canvas(raw_image: Image.Image, drawing_mode: str, stroke_width: int):
+    w, h = raw_image.size
+    width = min(w, 475)
+    height = width * h // w
 
+    mode = "rect"
+    stroke_color = "rgb(0, 0, 0)"
+
+    if drawing_mode == "sure_bg":
+        mode = "freedraw"
+        stroke_color = "rgb(0, 255, 0)"
+    elif drawing_mode == "sure_fg":
+        mode = "freedraw"
+        stroke_color = "rgb(255, 0, 0)"
+
+    canvas_result = st_canvas(
+        background_image=raw_image,
+        drawing_mode=mode,
+        fill_color="rgba(0, 0, 0, 0)",
+        stroke_width=stroke_width,
+        stroke_color=stroke_color,
+        width=width + 1,
+        height=height + 1,
+        key="full_app",
+    )
+
+    return canvas_result
 st.set_page_config(
-    page_title="Ứng dụng tách nền bằng thuật toán GrabCut",
-    page_icon=Image.open("./public/images/logo.png"),
+    page_title="Tách nền bằng thuật toán Grabcut",
+    page_icon=Image.open("./logo.png"),
     layout="wide",
     initial_sidebar_state="expanded",
 )
 
-st.title("Ứng dụng tách nền bằng thuật toán GrabCut")
+st.title("Tách nền bằng thuật toán GrabCut")
 
 with st.container(border=True):
-    display_guide()
+    st.markdown(
+        """
+        #### :material/search: Hướng dẫn sử dụng
+        1. Chọn ảnh cần tách nền.
+        2. Chọn **chế độ vẽ** và **độ dày nét vẽ**.
+        3. Vẽ hình chữ nhật lên ảnh để chọn vùng cần tách nền.
+        4. Chọn **chế độ vẽ** và vẽ lên ảnh để chỉ định vùng cần giữ lại hoặc loại bỏ.
+        5. Ấn nút `Tách nền` để xem kết quả.
+        """
+    )
 
 with st.container(border=True):
     uploaded_image = st.file_uploader(
-        ":material/image: Chọn hoặc kéo ảnh vào ô bên dưới", type=["jpg", "jpeg", "png"]
+        ":material/image: Chọn ảnh", type=["jpg", "jpeg", "png"]
     )
 
 if uploaded_image is not None:
--- pages/processing_image.py
+++ pages/processing_image.py
@@ -1,14 +1,169 @@
+import cv2
+import numpy as np
 from PIL import Image
 import streamlit as st
+from streamlit_drawable_canvas import st_canvas
 
 st.set_page_config(
     page_title="Ứng dụng xử lý ảnh",
-    page_icon=Image.open("./public/images/logo.png"),
+    page_icon=Image.open("./logo.png"),
     layout="wide",
     initial_sidebar_state="expanded",
 )
 
-from components.processing_image.display_app import display_app
+
+def display_flip_section(image: np.ndarray):
+    with st.form(key="flip_form"):
+        flip_options = ["Lật ngang", "Lật dọc", "Cả hai"]
+        flip_option = st.selectbox("Chọn hướng flip", flip_options)
+        submit = st.form_submit_button("Xử lý")
+
+    cols = st.columns(2)
+    cols[0].image(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), "Ảnh gốc", use_container_width=True)
+
+    if submit:
+        flip_image = image.copy()
+        if flip_option == "Lật dọc":
+            flip_image = cv2.flip(flip_image, 0)
+        elif flip_option == "Lật ngang":
+            flip_image = cv2.flip(flip_image, 1)
+        else:
+            flip_image = cv2.flip(flip_image, -1)
+
+        cols[1].image(cv2.cvtColor(flip_image, cv2.COLOR_BGR2RGB), "Ảnh sau khi flip", use_container_width=True)
+
+def display_st_canvas(raw_image: np.ndarray):
+    h, w = raw_image.shape[:2]
+    width = min(w, 475)
+    height = width * h // w
+    ratio = width / w
+
+    stroke_color = st.color_picker("Chọn màu vẽ:", "#FFFFFF")
+    stroke_width = st.slider("Chọn độ rộng bút vẽ:", 1, 10, 2)
+
+    canvas_result = st_canvas(
+        background_image=Image.fromarray(cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB)),
+        drawing_mode="rect",
+        fill_color="rgba(0, 0, 0, 0)",
+        stroke_width=stroke_width,
+        stroke_color=stroke_color,
+        width=width + 1,
+        height=height + 1,
+        key="rect_canvas",
+    )
+
+    return canvas_result, ratio
+
+def display_cropping_section(image: np.ndarray):
+    cols = st.columns(2)
+    with cols[0]:
+        canvas_result, ratio = display_st_canvas(image)
+
+    objects = canvas_result.json_data["objects"]
+
+    if objects is None or len(objects) == 0:
+        st.warning("Chọn vùng cần cropping")
+    elif len(objects) > 1:
+        st.warning("Chỉ chọn một vùng cần cropping")
+    else:
+        obj = objects[0]
+        left = int(obj["left"] / ratio)
+        top = int(obj["top"] / ratio)
+        width = int(obj["width"] / ratio)
+        height = int(obj["height"] / ratio)
+        cropped_image = image.copy()
+        cropped_image = cropped_image[top:top + height, left:left + width]
+        cols[1].image(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB), "Ảnh sau khi cắt", use_container_width=True)
+
+def display_colorspace_section(image: np.ndarray):
+    with st.form(key="colorspace_form"):
+        colorspace = st.selectbox("Chọn không gian màu", ["RGB", "HSV", "GRAY","CrCb"])
+        submit = st.form_submit_button("Xử lý")
+
+    cols = st.columns(2)
+    cols[0].image(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), "Ảnh gốc (RGB)", use_container_width=True)
+
+    if submit:
+        colorspace_image = image.copy()
+        if colorspace == "RGB":
+            colorspace_image = cv2.cvtColor(colorspace_image, cv2.COLOR_BGR2RGB)
+        elif colorspace == "HSV":
+            colorspace_image = cv2.cvtColor(colorspace_image, cv2.COLOR_BGR2HSV)
+        elif colorspace == "GRAY":
+            colorspace_image = cv2.cvtColor(colorspace_image, cv2.COLOR_BGR2GRAY)
+        elif colorspace == "CrCb":
+            colorspace_image = cv2.cvtColor(colorspace_image, cv2.COLOR_BGR2YCrCb)
+
+        cols[1].image(colorspace_image, "Ảnh sau khi biến đổi colorspace", use_container_width=True)
+
+def display_translation_section(image: np.ndarray):
+    with st.form(key="translation_form"):
+        tx = st.slider("Chọn tịnh tiến theo trục x",-image.shape[0], image.shape[0], 0)
+        ty = st.slider("Chọn tịnh tiến theo trục y", -image.shape[1], image.shape[1], 0)
+        submit = st.form_submit_button("Xử lý")
+
+    cols = st.columns(2)
+    cols[0].image(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), "Ảnh gốc", use_container_width=True)
+
+    if submit:
+        translation_image = image.copy()
+        # ma trận chuyển dịch
+        translation_matrix = np.float32([[1, 0, tx], [0, 1, ty]])
+        #ma trận tịch tiến
+        translation_image = cv2.warpAffine(translation_image, translation_matrix, (image.shape[1], image.shape[0]))
+
+        cols[1].image(cv2.cvtColor(translation_image, cv2.COLOR_BGR2RGB), "Ảnh sau khi tịnh tiến",use_container_width=True)
+
+def display_rotation_section(image: np.ndarray):
+    with st.form(key="roration_form"):
+        angle = st.slider("Góc quay", -180, 180, 0)
+        submit = st.form_submit_button("Xử lý")
+
+    cols = st.columns(2)
+    cols[0].image(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), "Ảnh gốc", use_container_width=True)
+
+    if submit:
+        rotation_image = image.copy()
+        h, w = rotation_image.shape[:2]
+        center = (w // 2, h // 2)
+        rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+        rotation_image = cv2.warpAffine(rotation_image, rotation_matrix, (w, h))
+
+        cols[1].image(cv2.cvtColor(rotation_image, cv2.COLOR_BGR2RGB), "Ảnh sau khi rotation", use_container_width=True)
+def display_app():
+    st.write(
+        """
+        - Ứng dụng dụng sử lý ảnh bao gồm: "Colorspace","Cropping","Rotation","Translation","Flip"
+        """
+    )
+    with st.container(border=True):
+        technique = st.selectbox("Chọn kỹ thuật cần thực hiện",
+                                 ["Colorspace","Cropping","Rotation","Translation","Flip"])
+        uploaded_file = st.file_uploader("Chọn một ảnh cần xử lý", type=["jpg", "png"], accept_multiple_files=False)
+
+    if uploaded_file is not None:
+        switcher = {
+            "Flip": display_flip_section,
+            "Rotation": display_rotation_section,
+            "Colorspace": display_colorspace_section,
+            "Translation": display_translation_section,
+            "Cropping": display_cropping_section
+        }
+        image_pil = Image.open(uploaded_file)
+        image = np.array(image_pil)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if technique == "Flip":
+            display_flip_section(image)
+        elif technique == "Rotation":
+            display_rotation_section(image)
+        elif technique == "Colorspace":
+            display_colorspace_section(image)
+        elif technique == "Translation":
+            display_translation_section(image)
+        elif technique == "Cropping":
+            display_cropping_section(image)
+        else:
+            st.write("Tính năng đang phát triển")
 
 st.title("Ứng dụng xử lý ảnh")
 display_app()
